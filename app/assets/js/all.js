626b013e7e07a85035e145adf37239bffbb319f5
Bearkern <haolin1224@gmail.com>
2022-09-06 01:16:20 +0800

    recommend course section

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,17 @@
+const swiper = new Swiper(".programming-language", {
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  // autoplay: {
+  //   delay: 3000,
+  //   pauseOnMouseEnter: true,
+  // },
+  breakpoints: {
+    992: {
+      spaceBetween: 16,
+    }
+  }
+});
+
 // $(function() {
 //   const elem = document.querySelector('input[name="formDate"]');
 //   const datepicker = new Datepicker(elem, {
