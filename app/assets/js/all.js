48c64e4c38b7cccf43192ed00fc7c974b32d892d
Bearkern <haolin1224@gmail.com>
2022-09-05 14:50:52 +0800

    finish index header RWD

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,7 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+// $(function() {
+//   const elem = document.querySelector('input[name="formDate"]');
+//   const datepicker = new Datepicker(elem, {
+//     language: 'zh-TW',
+//     buttonClass: 'btn',
+//   });
+// });
