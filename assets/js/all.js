23bb0794a4f7a8a629b70cec605356d0f7572fe4
Bearkern <haolin1224@gmail.com>
2022-09-08 01:08:08 +0800

    Update 2022-09-07T17:08:05.239Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,33 +1,31 @@
 "use strict";
 
-var swiperProgramming = new Swiper(".programming-language", {
-  slidesPerView: "auto",
-  spaceBetween: 8,
-  // autoplay: {
-  //   delay: 3000,
-  //   pauseOnMouseEnter: true,
-  // },
-  breakpoints: {
-    992: {
-      spaceBetween: 16
+$(function () {
+  var swiperProgramming = new Swiper(".programming-language", {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    breakpoints: {
+      992: {
+        spaceBetween: 16
+      }
     }
-  }
+  });
+  var swiperFeedback = new Swiper(".feedback-content", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".feedback-btn-next",
+      prevEl: ".feedback-btn-prev"
+    },
+    pagination: {
+      el: ".feedback-pagination",
+      clickable: true
+    }
+  });
+  var elem = document.querySelector('input[id="reservationDate"]');
+  var datepicker = new Datepicker(elem, {
+    autohide: true,
+    language: 'zh-TW',
+    buttonClass: 'btn'
+  });
 });
-var swiperFeedback = new Swiper(".feedback-content", {
-  cssMode: true,
-  navigation: {
-    nextEl: ".feedback-btn-next",
-    prevEl: ".feedback-btn-prev"
-  },
-  pagination: {
-    el: ".feedback-pagination",
-    clickable: true
-  }
-}); // $(function() {
-//   const elem = document.querySelector('input[name="formDate"]');
-//   const datepicker = new Datepicker(elem, {
-//     language: 'zh-TW',
-//     buttonClass: 'btn',
-//   });
-// });
 //# sourceMappingURL=all.js.map
