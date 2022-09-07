335f1518d33e03c4c985507dc620c42eac9ea495
Bearkern <haolin1224@gmail.com>
2022-09-08 01:07:44 +0800

    finish enroll process pages

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,33 +1,30 @@
-const swiperProgramming = new Swiper(".programming-language", {
-  slidesPerView: "auto",
-  spaceBetween: 8,
-  // autoplay: {
-  //   delay: 3000,
-  //   pauseOnMouseEnter: true,
-  // },
-  breakpoints: {
-    992: {
-      spaceBetween: 16,
+$(function() {
+  const swiperProgramming = new Swiper(".programming-language", {
+    slidesPerView: "auto",
+    spaceBetween: 8,
+    breakpoints: {
+      992: {
+        spaceBetween: 16,
+      }
     }
-  }
-});
+  });
 
-const swiperFeedback = new Swiper(".feedback-content", {
-  cssMode: true,
-  navigation: {
-    nextEl: ".feedback-btn-next",
-    prevEl: ".feedback-btn-prev",
-  },
-  pagination: {
-    el: ".feedback-pagination",
-    clickable: true,
-  },
-});
+  const swiperFeedback = new Swiper(".feedback-content", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".feedback-btn-next",
+      prevEl: ".feedback-btn-prev",
+    },
+    pagination: {
+      el: ".feedback-pagination",
+      clickable: true,
+    },
+  });
 
-// $(function() {
-//   const elem = document.querySelector('input[name="formDate"]');
-//   const datepicker = new Datepicker(elem, {
-//     language: 'zh-TW',
-//     buttonClass: 'btn',
-//   });
-// });
+  const elem = document.querySelector('input[id="reservationDate"]');
+  const datepicker = new Datepicker(elem, {
+    autohide: true,
+    language: 'zh-TW',
+    buttonClass: 'btn',
+  });
+});
